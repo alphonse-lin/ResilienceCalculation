1bf14a3df0bd2b6803299d6d8e8e1308b7a4a1f1
xuhui lin <xlin0541@outlook.com>
2023-03-09 20:35:22 +0000

    Update plot.py

--- utilis/plot.py
+++ utilis/plot.py
@@ -1,6 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from osgeo import gdal
+
+def readtif(input_path):
+    dataset = gdal.Open(input_path)
+    band1 = dataset.GetRasterBand(1)
+    b1 = band1.ReadAsArray()
+    return b1
 
 def show(data):
     plt.figure()
@@ -15,6 +22,10 @@ def save(data, path):
 if __name__ == '__main__':
     asc_dir=r'data\output\asc'
     img_dir=r'data\output\image'
+
+    # path=r'data\part_london\part\TQ27ne_DTM_1m.tif'
+    # arr=readtif(path)
+    # show(arr)
 
     files=os.listdir(asc_dir)
     plt.figure(figsize=(10,10),dpi=300)
